36f2e6c18c9bd627f394200085ffc5b90c210ed8
Mohd Shahzil <130140298+MohdShahzil29@users.noreply.github.com>
2024-11-25 09:57:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.use(fileUpload({ useTempFiles: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) =>{
+  res.send("Welcome")
+})
+
 // Routes
 app.use("/auth/api", authRoutes);
 app.use("/api/post", postRoutes);
